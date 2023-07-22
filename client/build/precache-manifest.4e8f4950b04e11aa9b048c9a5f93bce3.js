75ae73cfa9f7141f6e41f57f8552f6972f22f51c
Richards-Richie <danderichards360@gmail.com>
2023-07-22 16:52:42 +0530

    MemoGram

---
+++ client/build/precache-manifest.4e8f4950b04e11aa9b048c9a5f93bce3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a63c1707506fcd62608b97341fcb58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "758285e8c647b855dcdc",
+    "url": "/static/css/main.58869faf.chunk.css"
+  },
+  {
+    "revision": "55ebaf41efdb3e6a2771",
+    "url": "/static/js/2.a3c5edb9.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.a3c5edb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758285e8c647b855dcdc",
+    "url": "/static/js/main.e6912433.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.e6912433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "afbfa2f08597fcd4031064f287fe456c",
+    "url": "/static/media/logo192.afbfa2f0.png"
+  }
+]);
